e6409a47e251541d02875984e37dc35f4b0be983
Paul Sermon <paul.sermon@gemfair.com>
2024-12-19 10:35:06 +0000

    Added a missing tweak from an earlier version

--- graphene_django_extras/converter.py
+++ graphene_django_extras/converter.py
@@ -335,7 +335,8 @@ def convert_many_rel_to_djangomodel(field, registry=None, input_flag=None, neste
                 )
             else:
                 return DjangoListField(
-                    _type, required=is_required(field) and input_flag == "create"
+                    _type, required=is_required(field) and input_flag == "create",
+                    description=field.help_text or field.verbose_name,
                 )
 
     return Dynamic(dynamic_type)
